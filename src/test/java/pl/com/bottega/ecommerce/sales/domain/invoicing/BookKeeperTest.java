2e3e52aab5842391b3d8986aac94bfaf76e6791d
NickiMina <olkasia@poczta.one.pl>
2021-04-09 18:25:27 +0200

    test sprawdzajcy fakture z dwiema pozycjami

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -58,6 +59,33 @@ class BookKeeperTest {
         bookKeeper.issuance(request,taxPolicy);
 
         assertEquals(invoice.getItems().size(),1);
+    }
+
+    @Test
+    void invoiceRequestReturnTwoItems() {
+        ClientData clientData= new ClientData(Id.generate(),"oneClientTwoItems");
+        InvoiceRequest request = new InvoiceRequest(clientData);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(ProductType.STANDARD,new Money(5))).thenReturn(new Tax(new Money(0.05),"5%"));
+        when(taxPolicy.calculateTax(ProductType.FOOD,new Money(16))).thenReturn(new Tax(new Money(0.025),"2.5%"));
+
+        ProductData productData=mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        ProductData productDataFood=mock(ProductData.class);
+        when(productDataFood.getType()).thenReturn(ProductType.FOOD);
+
+        RequestItem requestItem = new RequestItem(productData,3,new Money(5));
+        request.add(requestItem);
+        RequestItem requestItem2 = new RequestItem(productDataFood,7,new Money(16));
+        request.add(requestItem2);
+
+        Invoice invoice = new Invoice(Id.generate(),clientData);
+        when(factory.create(clientData)).thenReturn(invoice);
+        bookKeeper.issuance(request,taxPolicy);
+
+        Mockito.verify(taxPolicy,times(1)).calculateTax(ProductType.STANDARD, new Money(5));
+        Mockito.verify(taxPolicy,times(1)).calculateTax(ProductType.FOOD,new Money(16));
     }
 
 
