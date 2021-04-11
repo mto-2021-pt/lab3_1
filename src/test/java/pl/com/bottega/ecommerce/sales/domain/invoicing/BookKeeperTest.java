ebb2eb133b17b1a0a4a131f3917b2c56d9a1fb87
kamil <222574@edu.p.lodz.pl>
2021-04-11 22:55:20 +0200

    Added test for invoice with one item

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,57 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
+    @Mock
+    private InvoiceFactory factory;
+    @Mock
+    private TaxPolicy taxPolicy;
+
+    private BookKeeper bookKeeper;
+    private InvoiceRequest invoiceRequest;
+    private ClientData clientData;
 
     @BeforeEach
     void setUp() throws Exception {
+        bookKeeper = new BookKeeper(factory);
+        clientData = new ClientData(Id.generate(),"client");
+        invoiceRequest = new InvoiceRequest(clientData);
+        taxPolicy = mock(TaxPolicy.class);
 
     }
+
 
     @Test
-    void test() {
-        fail("not implemented");
+    void InvoiceWithOneItemSizeTest() {
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(new Tax(Money.ZERO,"desc"));
+        
+        ProductData productData=mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+        RequestItem requestItem = new RequestItem(productData,1,Money.ZERO);
+        invoiceRequest.add(requestItem);
+
+        Invoice invoice = new Invoice(Id.generate(),clientData);
+        when(factory.create(clientData)).thenReturn(invoice);
+
+        bookKeeper.issuance(invoiceRequest,taxPolicy);
+        assertEquals(invoice.getItems().size(),1);
     }
+
 
 }
