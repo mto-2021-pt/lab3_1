c6187539efc8aef37be3c7d23f7cb64406f2eec5
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-04-20 02:49:48 +0200

    Added state and behaviour tests. Fixed tests setup.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,9 +1,10 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,28 +24,37 @@ class BookKeeperTest {
     private InvoiceFactory factory;
     @Mock
     private TaxPolicy tp;
-
     private BookKeeper bk;
     private ClientData cd;
     private InvoiceRequest ir;
     private Product p;
     private RequestItem ri;
 
-
     @BeforeEach
     void setUp() throws Exception {
-        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
-        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
         bk=new BookKeeper(factory);
         cd=new ClientData(Id.generate(),"default");
-        ir=new InvoiceRequestBuilder().build();
+        ir=new InvoiceRequestBuilder().withClient(cd).build();
         p=new ProductBuilder().build();
-        ri=new RequestItemBuilder().build();
+        ri=new RequestItemBuilder().withProductData(p.generateSnapshot()).withQuantity(1).build();
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
     }
 
     @Test
-    void test() {
-        fail("not implemented");
+    void testsuccessful_oneiteminvoicerequest(){
+        ir.add(ri);
+        assertEquals(1,bk.issuance(ir,tp).getItems().size());
     }
+
+    @Test
+    void testsuccessful_twoitemsinvoicerequest(){
+        ir.add(ri);
+        ir.add(ri);
+        bk.issuance(ir,tp);
+        verify(tp,times(2)).calculateTax(ProductType.STANDARD,Money.ZERO);
+    }
+
+
 
 }
