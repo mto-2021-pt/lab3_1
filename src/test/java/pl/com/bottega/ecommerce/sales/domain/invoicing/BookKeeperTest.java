cf4c4a485a4eb171ace97eda944ad85d11164b17
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-04-20 03:13:07 +0200

    Added 2 new state and 2 new behaviour tests. Moved stubs from setup to tests.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,7 +1,6 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Assertions;
@@ -24,6 +23,7 @@ class BookKeeperTest {
     private InvoiceFactory factory;
     @Mock
     private TaxPolicy tp;
+
     private BookKeeper bk;
     private ClientData cd;
     private InvoiceRequest ir;
@@ -37,24 +37,57 @@ class BookKeeperTest {
         ir=new InvoiceRequestBuilder().withClient(cd).build();
         p=new ProductBuilder().build();
         ri=new RequestItemBuilder().withProductData(p.generateSnapshot()).withQuantity(1).build();
-        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
-        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
     }
 
     @Test
-    void testsuccessful_oneiteminvoicerequest(){
+    void state_testsuccessful_oneiteminvoicerequest(){
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
         ir.add(ri);
         assertEquals(1,bk.issuance(ir,tp).getItems().size());
     }
 
     @Test
-    void testsuccessful_twoitemsinvoicerequest(){
+    void behaviour_testsuccessful_twoitemsinvoicerequest(){
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
         ir.add(ri);
         ir.add(ri);
         bk.issuance(ir,tp);
         verify(tp,times(2)).calculateTax(ProductType.STANDARD,Money.ZERO);
     }
 
+    @Test
+    void state_testsuccessful_noitemsinvoicerequest(){
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        assertEquals(0, bk.issuance(ir, tp).getItems().size());
+    }
+
+    @Test
+    void behaviour_testsuccessful_threeitemsinvoicerequest(){
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
+        ir.add(ri);
+        ir.add(ri);
+        ir.add(ri);
+        bk.issuance(ir,tp);
+        verify(tp,times(3)).calculateTax(ProductType.STANDARD,Money.ZERO);
+    }
+
+    @Test
+    void behaviour_testunsuccessful_invoicerequestisnull(){
+        ir=null;
+        assertThrows(NullPointerException.class,()->bk.issuance(ir,tp));
+    }
+
+    @Test
+    void state_testsuccessful_oneiteminvoicerequest_requestitemquantityisnegative(){
+        when(factory.create(cd)).thenReturn(new Invoice(Id.generate(),cd));
+        when(tp.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO,"default"));
+        ri=new RequestItemBuilder().withQuantity(-1).withProductData(p.generateSnapshot()).build();
+        ir.add(ri);
+        assertEquals(1, bk.issuance(ir, tp).getItems().size());
+    }
 
 
 }
